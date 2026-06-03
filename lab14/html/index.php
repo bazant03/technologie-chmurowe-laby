af8ae04f7097e6cc7ba52b091bbef052a1d8990a
bazant03 <bazant0303@gmail.com>
2026-06-03 20:31:27 +0200

    php with nginx in one network

---
+++ lab14/html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stronka</title>
+</head>
+<body>
+    <?php
+        echo '<p>Testowa strona</p>';
+    ?>
+</body>
+</html>
